096eab67e9220f585cb3c5e23d75009f123c3d18
Drew Volz <volz.drew@gmail.com>
2021-02-09 20:35:49 -0700

    Pull typealiases and constants out into separate file

---
+++ suitcase/constants.swift
@@ -0,0 +1,17 @@
+//
+//  constants.swift
+//  suitcase
+//
+//  Created by Drew Volz on 2/8/21.
+//
+
+import Foundation
+
+// MARK: Types
+
+typealias FileNames = [String]
+typealias FileContents = [String: String]
+
+// MARK: Shared
+
+let fileManager = FileManager.default
